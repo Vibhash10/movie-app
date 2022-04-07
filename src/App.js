ad81d9c0e2d50bcc4f4748ac483d6b799cdc5642
Vibhash Ray <ranjanrayvibhash@gmail.com>
2022-04-07 17:32:45 +0530

    Added basic routing for the application

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import MovieDetail from "./components/MovieDetail/MovieDetail";
-import PageNotFound from "./components/PateNotFound/PageNotFound";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 function App() {
   return (
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/apis/movieApi";
+import { APIKEY } from "../../common/apis/MovieApiKey";
 
 const Home = () => {
-  return <div>Home</div>;
+  const movieText = "American";
+
+  const fetchMovies = async () => {
+    const response = await movieApi
+      .get(`?apiKey=${APIKEY}&s=${movieText}&type=movie`)
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log("The respponse from API call", response);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <div className="banner-img">Home</div>
+      <MovieListing />
+    </div>
+  );
 };
 
 export default Home;
