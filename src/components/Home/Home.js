05a84f51e7dd9aed3b67703577472a693519c994
Vibhash Ray <ranjanrayvibhash@gmail.com>
2022-04-09 02:43:37 +0530

    Added redux for the application

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/apis/movieApi";
 import { APIKEY } from "../../common/apis/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
   const movieText = "American";
+  const dispatch = useDispatch();
 
   const fetchMovies = async () => {
     const response = await movieApi
@@ -13,6 +16,7 @@ const Home = () => {
         console.log(error);
       });
     console.log("The respponse from API call", response);
+    dispatch(addMovies(response.data));
   };
 
   useEffect(() => {
---
+++ src/features/movies/movieSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  movies: [],
+};
+
+export const movieSlice = createSlice({
+  name: "moviesName",
+  initialState,
+  reducers: {
+    addMovies: (state, { payload }) => {
+      state.movies = payload;
+    },
+  },
+});
+
+export const { addMovies } = movieSlice.actions;
+export const getAllMovies = (state) => state.moviesName.movies;
+export default movieSlice.reducer;
